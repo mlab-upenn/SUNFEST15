ab01f3798c1320f41133101dc5c5d22c7bcd000e
madhurbehl <behl.madhur@gmail.com>
2015-06-03 16:39:44 -0400

    Initial commit: data and matlab code

---
+++ Matlab Code/CleanXY.m
@@ -0,0 +1,21 @@
+function [cX,cY,len,loss] = CleanXY(X,Y,sigmah,sigmal)
+% Clean the training data of missing data nd outliers.
+
+Ybar = mean(Y);
+Ydev = std(Y);
+Ylimh = Ybar+(sigmah*Ydev);
+Yliml = Ybar-(sigmal*Ydev);
+
+Yidx = find((Y==0)|(Y>=Ylimh)|(Y<=Yliml));
+
+X(Yidx,:)=[];
+Y(Yidx) = [];
+
+cX = X;
+cY = Y;
+
+len = length(cY);
+loss = (length(Y)-len)/length(Y);
+
+end
+
