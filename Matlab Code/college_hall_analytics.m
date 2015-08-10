54e3d0551395a8a648ac98dd69d10fdfba67f9c0
sxg611 <santiti28@gmail.com>
2015-08-10 16:28:37 -0400

    College analytics with outlier removal, interpolation, averages, and boxplots

--- Matlab Code/college_hall_analytics.m
+++ Matlab Code/college_hall_analytics.m
@@ -1,9 +1,16 @@
+% clear the command line.
+clc
+% clear the workspace.
+clear 
+% close any prev figures.
+close all
 
-Data = csv2matPenn('1 _College_Hall_2.csv','College_Hall');  
+
+Data = csv2matPenn('1 _College_Hall_2.csv','College_Hall'); 
 Numdata = Data(2:end,1:end);
 Tabledata = cell2mat(Numdata);
 
-% indexes csv file for data 
+% indexes csv file for data and assigns it to variables 
 dom = Tabledata(:,3);
 tod = Tabledata(:,4);
 tempC = Tabledata(:,5);
@@ -62,6 +69,19 @@ Ytest = iY(trlen+1:end);
 range = max(Ytest)-min(Ytest);
 bar = mean(Ytest);
 
+% plot the training data features.
+figure();
+for jj=1:length(colnames);
+    
+    subplot(ceil(length(colnames)/2),2,jj);
+    plot(Xtrain(:,jj));
+    str = colnames(jj);
+    title(str);
+    grid on;
+end
+
+ %% Start Tree Regression
+disp('Learning Regression Tree...');
 
 % minimium number of leaf node observations. This is a stopping cirtera for
 % the recursive partitionioning algorithm used by the tree.
@@ -74,167 +94,146 @@ tic
 college_hall_tree14 = RegressionTree.fit(Xtrain,Ytrain,'PredictorNames',colnames,'ResponseName','Total Power','CategoricalPredictors',catcol,'MinLeaf',minleaf);
 toc
 
+% You can view the tree by uncommenting the line below.
+%view(largetree12,'mode','graph');
 
-leaf_index = find((college_hall_tree14.Children(:,1)==0)&(college_hall_tree14.Children(:,2)==0)); % index of node that is a leaf
-numleafs = length(leaf_index); % number of leaf nodes 
-fprintf('The tree has %d leaf nodes \n',numleafs);
+% predict on training and testing data and plot the fits
+Yfit = predict(college_hall_tree14,college_hall_tree14.X);
 
-% returns node, which is the index of of training samples in each leaf
-[Yfit,node] = resubPredict(college_hall_tree14); 
-Y_mean = zeros(1,numleafs);
+% compute NRMSE for the traingin period: also referred to as resubstitution
+% error.
+[a,b]=rsquare(college_hall_tree14.Y,Yfit);
+fprintf('Training RMSE(W): %.2f, R2: %.3f, RMSE/peak: %.4f, NRMSD: %.2f \n\n'...
+    ,b,a,(b/max(college_hall_tree14.Y)),(100*b/(max(college_hall_tree14.Y)-min(college_hall_tree14.Y))));
 
-% Training Data that the tree uses
-%Different than Ytrain because does not use all data points to train model
-TreeY = college_hall_tree14.Y;
-TreeX = college_hall_tree14.X;
 
-for i=1:numleafs
-    
-    % find indices of samples which end up in this leaf
-    ST(i).leaves = {find(node==leaf_index(i))};
-    
-    % prediction at the leaf
-    ST(i).mean = college_hall_tree14.NodeMean(leaf_index(i));
-    Y_mean(i) = ST(i).mean;
-    % find the training samples which contribute to this leaf (support)
-    
-    ST(i).xdata = {TreeX(ST(i).leaves{1,1},:)};
-    ST(i).xlength = length(cell2mat(ST(i).xdata));
-    
-    % find the training labels which contribute to this leaf
-    ST(i).ydata = {TreeY(ST(i).leaves{1,1})};
-    ST(i).ylength = length(cell2mat(ST(i).ydata));
-    
-    % finds the confidence interval for data points in each leaf
-    ydata_array = cell2mat(ST(i).ydata);
-    SEM = std(ydata_array)/sqrt(length(ydata_array));
-    ts = tinv([.025 .0975],length(ydata_array)-1);
-    ST(i).CI = mean(ydata_array)+ ts*SEM;
+ AnalyticsFunction (college_hall_tree14,5);
 
+
+%% Improve the tree by using k-fold cross validation
+disp('Learning a cross validated tree...');
+
+% specify number of folds.
+kf = 10;
+
+% train cross-validated trees.
+tic
+b101treeCV = RegressionTree.fit(Xtrain,Ytrain,'PredictorNames',colnames,...
+    'ResponseName','Total Power','CategoricalPredictors',catcol,...
+    'MinLeaf',minleaf,'CrossVal','on','KFold',kf); % default is 10-fold
+toc
+
+% obtain predictions on the traiing set.
+YfitCV = kfoldPredict(b101treeCV);
+
+% compute resubstitution error for the CV trees
+[a,b]=rsquare(college_hall_tree14.Y,YfitCV);
+fprintf('Cross Validated Training RMSE(W): %.2f, R2: %.3f, RMSE/peak %.4f, NRMSD: %0.2f \n\n'...
+    ,b,a,(b/max(college_hall_tree14.Y)),(100*b/(max(college_hall_tree14.Y)-min(college_hall_tree14.Y))));
+
+
+%% Random Forests
+ disp('Learning a Random Forest...');
+% leaf = [5 10 50 100];
+% col = 'rbcmyk';
+% figure();
+% for i=1:length(leaf)
+%     brf = TreeBagger(200,Xtrain,Ytrain,'Method','regression','OOBPred','On','OOBVarImp','on',...
+%         'CategoricalPredictors',catcol,'MinLeaf',leaf(i));
+%     plot(sqrt(oobError(brf)),col(i));
+%     hold on;
+% end
+% xlabel 'Number of Grown Trees';
+% ylabel 'Root Mean Squared Error' ;
+% legend({'5','10' '50' '100'},'Location','NorthEast');
+% hold off;
+% 
+% figure();
+% barh(brf.OOBPermutedVarDeltaError);
+% xlabel 'Feature' ;
+% ylabel 'Out-of-Bag Feature Importance';
+% set(gca,'YTickLabel',colnames);
+
+% train a random forest with 500 trees.
+tic;
+B = TreeBagger(500,Xtrain,Ytrain,'Method','regression','OOBPred','On','OOBVarImp','On',...
+      'MinLeaf',minleaf);
+toc;
+
+% plot feature importance.
+figure();
+barh(B.OOBPermutedVarDeltaError);
+xlabel 'Feature' ;
+ylabel 'Out-of-Bag Feature Importance';
+set(gca,'YTickLabel',colnames);
+
+% figure();
+% plot(sqrt(oobError(B)));
+
+% obtian predictions on training data
+Ybag = predict(B,college_hall_tree14.X);
+
+% resub error
+[a,b]=rsquare(college_hall_tree14.Y,Ybag);
+fprintf('Random Forests Training RMSE(W): %.2f, R2: %.3f, RMSE/peak %0.4f, NRMSD: %0.2f \n\n'...
+    ,b,a,(b/max(college_hall_tree14.Y)),(100*b/(max(college_hall_tree14.Y)-min(college_hall_tree14.Y))));
+
+
+
+%%
+
+% plot the prediction performance on the training data for all the methods.
+figure;
+plot(college_hall_tree14.Y), hold on
+plot(Yfit,'y');
+plot(YfitCV,'r');
+plot(Ybag,'c');
+grid on;
+legend('Ground Truth','Single Tree','CV Tree','Forest');
+title('Training');
+hold off;
+
+% now obtian prediction ont he test set. This is really the value we care
+% about since we are evaluationg thwe prediction performance on data that
+% the algorithm has never seen before.
+Ypredict = predict(college_hall_tree14,Xtest);
+
+% RMSE on the test set
+[a,b]=rsquare(Ytest,Ypredict);
+fprintf('Testing RMSE(W): %.2f, R2: %.3f, RMSE/peak: %.4f, NRMSD: %.2f \n'...
+    ,b,a,(b/max(Ytest)),(100*b/bar));
+
+% prediction on test data for cross validated (CV) trees.
+YpredictCVk=zeros(length(Xtest),kf);
+for ii=1:kf
+    YpredictCVk(:,ii)=predict(b101treeCV.Trained{ii,1},Xtest);
 end
-%Creates a Scatterplot with Y mean value at each leave on the X axis and
-% the leaf index on the Y axis
-Xaxis = linspace (1,numleafs,numleafs);
-Scatterplot = scatter(Y_mean,Xaxis,10);
-xlabel 'Y mean for each leaf';
-ylabel 'leaf index';
+YpredictCV = sum(YpredictCVk,2)/kf;
 
-%Specifiy Bin width by setting a range of Y values want to query
-Binwidth = 5;
-Y_Center = 35; 
-Ymax = Y_Center+Binwidth ;
-Ymin= Y_Center-Binwidth ;
+% RMSE on the test set
+[a,b]=rsquare(Ytest,YpredictCV);
+fprintf('Cross Validated Testing RMSE(W): %.2f, R2: %.3f, RMSE/peak %.4f, NRMSD: %0.2f \n'...
+    ,b,a,(b/max(Ytest)),(100*b/bar));
 
-%creates vector with zeros as long as the number of leafs 
-% Will add leafs that lie within specified Bin range
-% then remove extra zeros
-Data_index = zeros(1,numleafs);
+% Do the same for random forests
+Ybagt = predict(B,Xtest);
+% RMSE
+[a,b]=rsquare(Ytest,Ybagt);
+fprintf('RF (Testing) RMSE(W): %.2f, R2: %.3f, RMSE/peak %.4f, NRMSD: %0.2f \n\n'...
+    ,b,a,(b/max(Ytest)),(100*b/bar));
 
-%Find the index of the leaf which are within specified Bin width and adds
-%them to Data_index
-for ii=1:numleafs
-    if (ST(ii).mean >= Ymin) && (ST(ii).mean <= Ymax); 
-    Data_index(ii) = ii;  % vector with leafs that lie within 
-    end
-end    
-%Removes zeros from Data_index, leaving only list with indeces of leafs
-%within range 
-Data_index = Data_index(Data_index ~= 0);
-
-% scalar that will count the total number of data points in all the leafs
-% that lie within the specified bin range 
-Total_Points = 0;
-
-% loops through the vector with leafs
-for j= 1:length(Data_index);    
-    leaf_number = Data_index(j); % leaf_number in the node number of the leaf 
-    RelData_Points = cell2mat(ST(leaf_number).leaves);% array with Y data points in each leaf
-    for jj = 1:length(RelData_Points);  %for loop that will iterate through each data point and take each feature 
-        Xtree_index = RelData_Points(jj);% index of X data point
-        Train_row = TreeX(Xtree_index,:); % row taken from original Xtrain data with feature information
-        
-        % parses Xtrain data and creates data structure with feature
-        % information for each data point in each leaf
-        Q.Leaf(j).Point(jj).dom = Train_row(1,1);
-        Q.Leaf(j).Point(jj).tod = Train_row(1,2);
-        Q.Leaf(j).Point(jj).tempC = Train_row(1,3);
-        Q.Leaf(j).Point(jj).sol = Train_row(1,4);
-        Q.Leaf(j).Point(jj).occ = Train_row(1,5);
-        Q.Leaf(j).Point(jj).mon = Train_row(1,6);   % fix index on git
-        Q.Leaf(j).Point(jj).winspeed = Train_row(1,7);
-        Q.Leaf(j).Point(jj).windir = Train_row(1,8);
-        Q.Leaf(j).Point(jj).gusts = Train_row(1,9);
-        Q.Leaf(j).Point(jj).hum = Train_row(1,10);
-        Q.Leaf(j).Point(jj).dew = Train_row(1,11);
-        Q.Leaf(j).Point(jj).hdd = Train_row(1,12);
-        Q.Leaf(j).Point(jj).cdd = Train_row(1,13);
-        
-        Total_Points =  Total_Points + 1; % Counts that total number of data points that the loop iterates through
-    end   
-end
-
-    TotalSum_dom = 0;
-    TotalSum_tod = 0;
-    TotalSum_tempC = 0;
-    TotalSum_sol = 0;
-    TotalSum_occ = 0;
-    TotalSum_mon = 0;
-    TotalSum_winspeed = 0;
-    TotalSum_windir = 0;
-    TotalSum_gusts = 0;
-    TotalSum_hum = 0;
-    TotalSum_dew = 0;
-    TotalSum_hdd = 0;
-    TotalSum_cdd = 0;
-    
-%iterates through leaves and sums values for each feature
-% It is the total sum of values for each feature in data points that lie within the
-% specified bin range 
-for f= 1:length(Data_index);
-
-    TotalSum_dom = sum([Q.Leaf(f).Point(:).dom]) + TotalSum_dom;
-    TotalSum_tod = sum([Q.Leaf(j).Point(:).tod]) + TotalSum_tod ; 
-    TotalSum_tempC = sum([Q.Leaf(j).Point(:).tempC]) + TotalSum_tempC ;
-    TotalSum_sol = sum([Q.Leaf(j).Point(:).sol]) + TotalSum_sol ;
-    TotalSum_occ = sum([Q.Leaf(j).Point(:).occ]) + TotalSum_occ;
-    TotalSum_mon = sum([Q.Leaf(j).Point(:).mon]) + TotalSum_mon ;
-    TotalSum_winspeed = sum([Q.Leaf(j).Point(:).winspeed]) + TotalSum_winspeed;
-    TotalSum_windir = sum([Q.Leaf(j).Point(:).windir]) + TotalSum_winspeed ; 
-    TotalSum_gusts = sum([Q.Leaf(j).Point(:).gusts]) + TotalSum_gusts ;
-    TotalSum_hum = sum([Q.Leaf(j).Point(:).hum]) +TotalSum_hum ; 
-    TotalSum_dew = sum([Q.Leaf(j).Point(:).dew]) + TotalSum_dew ;
-    TotalSum_hdd = sum([Q.Leaf(j).Point(:).hdd]) + TotalSum_hdd  ;
-    TotalSum_cdd = sum([Q.Leaf(j).Point(:).cdd]) + TotalSum_cdd ;
-end
-
-% Calculates average by diving the sum off features divided by
-% total number of data points that lie within the Bin
-    avg_dom = (TotalSum_dom ./ Total_Points)
-    avg_tod = (TotalSum_tod ./ Total_Points)
-    avg_tempC = (TotalSum_tempC ./ Total_Points)
-    avg_sol = (TotalSum_sol ./ Total_Points)
-    avg_occ =  (TotalSum_occ ./ Total_Points)
-    avg_mon =  (TotalSum_mon ./ Total_Points)
-    avg_winspeed = (TotalSum_winspeed ./ Total_Points)
-    avg_windir = (TotalSum_windir ./ Total_Points)
-    avg_gusts = (TotalSum_gusts ./ Total_Points)
-    avg_hum = (TotalSum_hum ./ Total_Points)
-    avg_dew = (TotalSum_dew ./ Total_Points)
-    avg_hdd = (TotalSum_hdd ./ Total_Points)
-    avg_cdd = (TotalSum_cdd ./ Total_Points)
-   
-    % Calculates support by dividing total data points in specified range
-    % by total number of points in all training data 
-    Support = Total_Points ./ length(TreeX);
-    
-    %fprintf('Average day of month: %.2f, Average Time of day: %.2f, Average Temp(C): %.2f, Average Solar Radiation: %.2f, Average Occupancy: %.2f, Average Month: %.2f, Average Windspeed: %.2f, Avereage Windirection: %.2f, Average Gusts: %.2f,Average Humidity: %.2f, Average Dew Point: %.2f, Average Heating Days: %.2f, Average Cooling Days: %.2f \n\n' '
-    
-
-
-
-
-
-
+% plot the prediciton vs ground truth comparison for each method on the
+% test set.
+figure;
+plot(Ytest), hold on
+axis([0 length(Ytest) 0 max(Ytest)+20]);
+plot(Ypredict,'y');
+plot(YpredictCV,'r');
+plot(Ybagt,'c');
+legend('Ground Truth','Single','CV','Forest');
+title('2015');
+grid on;
+hold off;
 
 
 
